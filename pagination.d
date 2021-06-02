5ee874b4b59595d89ecc162dbeab86facdf211aa
unknown <axelgrullon@outlook.com>
2021-06-02 14:33:56 -0400

    Current page conditions

--- pagination.d
+++ pagination.d
@@ -1,13 +1,30 @@
 import std.stdio;
+import std.math.ceil;
+import std.math;
+
 
 class Paginator{
 public:
-	int totalPages;
-	int currentPage;
+	int totalElements;
+	int currentPag = 1;
 	int maxPages = 7;
 	int pagesSize = 100;
 }
 
 void main(){
+
+	auto totalPages = int (ceil((float totalElements) / ((float pagesSize)
+// Verificando que estamos en rango apropiado y el elemento apropiado
+
+		if (currentPage < 1){
+
+			currentPage = 1;
+
+		}
+		else if (currentPage > totalPages){
+
+			currentPage = totalPages;
+
+		}
 
 }
